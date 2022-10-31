31f58680303c8a5f9def93476ff2e85dc738f377
xidef-fedix <109800756+xidef-fedix@users.noreply.github.com>
2022-10-31 20:43:46 +0300

    сделал final переменные

--- src/main/java/ru/fedko/project/springbootweb/controller/UserController.java
+++ src/main/java/ru/fedko/project/springbootweb/controller/UserController.java
@@ -10,7 +10,7 @@ import ru.fedko.project.springbootweb.service.UserService;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
--- src/main/java/ru/fedko/project/springbootweb/service/UserServiceImpl.java
+++ src/main/java/ru/fedko/project/springbootweb/service/UserServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private UserDao userDao;
+    private final UserDao userDao;
 
     public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
